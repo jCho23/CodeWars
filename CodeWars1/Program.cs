c2d9dfe15f1224bf6b95482d3a71ed3421a1bf70
JuneCho23 <junecho.ucb@gmail.com>
2017-03-29 09:36:59 -0700

    Code Wars

---
+++ CodeWars1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CodeWars1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int[] a = { 1, 2, 3, 4, 25 };
+			int[] b = { 16, 4, 1, 9, 625 };
+			int[] c = { 4, 1, 7, 5 };
+
+			Console.WriteLine($"comp(a , b) = {comp(a, b)}");
+			Console.WriteLine($"comp(a , c) = {comp(a, c)}");
+		}
+
+		public static bool comp(int[] a, int[] b)
+		{
+			if (a == null || b == null)
+				return false;
+
+
+			for (int i = 0; i < a.Length; i++)
+			{
+				bool doesBContainSquareOfA = false;
+
+				for (int j = 0; j < b.Length; j++)
+				{
+					if (a[i] * a[i] == b[j])
+					{
+						doesBContainSquareOfA = true;
+						break;
+					}
+				}
+
+				if (doesBContainSquareOfA == false)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+
+
+
+	}
+}
